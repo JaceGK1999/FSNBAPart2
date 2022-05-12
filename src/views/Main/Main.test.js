8b32d211739b17cdb899029c98696adf3f73f12b
Jacegk1999 <Jacegk1999@gmail.com>
2022-05-11 17:28:09 -0700

    working test to see is list works and renders

--- src/views/Main/Main.test.js
+++ src/views/Main/Main.test.js
@@ -1,18 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import App from '../App';
-
+import App from '../../App';
 
 describe('Home page renders without.', () => {
-    it('should load a list of people from R and M', async () => {
-      render(
-        <MemoryRouter>
-          <App />
-        </MemoryRouter>
-      );
-  
-      //when you open the page you will find a loading state that says 
-      
+  it('should load a list of people from R and M', async () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
 
-    });
+    //when you open the page you will find a loading state that says "Loading... Hopefully you dont have to look at this for to long...""
+    const loading = screen.getByText(
+      'Loading... Hopefully you dont have to look at this for to long...'
+    );
+    expect(loading).toBeInTheDocument();
+
+    // after the page loads you will see a list of people from rick and morty
+    const Character = await screen.findByText(
+      'Name: Abadango Cluster Princess (Alive)'
+    );
+    expect(Character).toBeInTheDocument();
   });
+});
