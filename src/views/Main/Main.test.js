8158ca2c2a7a4101d316c8d301bef487e2b7b127
Jacegk1999 <Jacegk1999@gmail.com>
2022-05-11 17:30:40 -0700

    setup second test, need to make it behavioral

--- src/views/Main/Main.test.js
+++ src/views/Main/Main.test.js
@@ -22,4 +22,22 @@ describe('Home page renders without.', () => {
     );
     expect(Character).toBeInTheDocument();
   });
+  it('should navigate to Morty Smith', async () => {
+    render(
+      <MemoryRouter initialEntries={['/2']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    //when you open the page you will find a loading state that says "Loading... Hopefully you dont have to look at this for to long...""
+    const loading = screen.getByText(
+      'Loading... Hopefully you dont have to look at this for to long...'
+    );
+    expect(loading).toBeInTheDocument();
+
+    // after the page loads you will see a list of people from rick and morty
+    const Character = await screen.findByText(
+      'Name: Abadango Cluster Princess (Alive)'
+    );
+  });
 });
